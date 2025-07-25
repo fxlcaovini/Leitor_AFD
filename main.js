7cc0e9ac7d709a9962a88d00a44f71c3aa53d138
fxlcaovini <vinnehp@hotmail.com>
2025-07-25 00:08:28 -0300

    commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow, shell } = require('electron');
+const { app, BrowserWindow, shell, dialog } = require('electron');
 const path = require('path');
-const { autoUpdater } = require('electron-updater'); // Adiciona auto-update
+const { autoUpdater } = require('electron-updater');
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -18,13 +18,11 @@ function createWindow () {
 
   win.loadFile('index.html');
 
-  // Intercepta novas janelas e abre no navegador
   win.webContents.setWindowOpenHandler(({ url }) => {
     shell.openExternal(url);
     return { action: 'deny' };
   });
 
-  // Intercepta cliques em links externos
   win.webContents.on('will-navigate', (event, url) => {
     const currentURL = win.webContents.getURL();
     if (url !== currentURL) {
@@ -32,11 +30,32 @@ function createWindow () {
       shell.openExternal(url);
     }
   });
+
+  // Notificações personalizadas de atualização
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox(win, {
+      type: 'info',
+      title: 'Atualização disponível',
+      message: 'Uma nova versão está disponível e será baixada automaticamente.',
+      buttons: ['OK']
+    });
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox(win, {
+      type: 'info',
+      title: 'Atualização pronta',
+      message: 'A atualização foi baixada. O aplicativo será reiniciado para concluir a atualização.',
+      buttons: ['Reiniciar agora']
+    }).then(() => {
+      autoUpdater.quitAndInstall();
+    });
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
-  autoUpdater.checkForUpdatesAndNotify(); // Verifica e aplica atualizações
+  autoUpdater.checkForUpdatesAndNotify();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
